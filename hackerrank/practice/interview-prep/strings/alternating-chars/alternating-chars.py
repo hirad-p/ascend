0e42f66a2f9742bb3e718be8e6ef045d57064726
hirad-p <hirad.pourtahmasbi@gmail.com>
2018-09-01 22:20:27 -0700

    Hackerrank Alternating Character

---
+++ hackerrank/practice/interview-prep/strings/alternating-chars/alternating-chars.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+# From https://www.hackerrank.com/challenges/alternating-characters
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s):
+    count = 0 # count of skips   
+    p = ""    # previous letter encountered
+    for c in s:
+        if c != p: 
+            p = c
+        else:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
